d063fc580ca774e8e3b0ec76005617f80d61de01
gustav <gustavuh0012@gmail.com>
2025-11-08 19:51:14 -0300

    feat: area fale conosco

---
+++ js/js.js
@@ -0,0 +1,40 @@
+
+
+            function validarFormulario() {
+            const nome = document.getElementById('nome').value.trim();
+            const email = document.getElementById('email').value.trim();
+            const telefone = document.getElementById('telefone').value.trim();
+            const motivo = document.getElementById('motivo').value.trim();
+           
+
+            const campos = [{ valor: nome, nome: 'Nome', elemento: document.getElementById('nome') },{ valor: email, nome: 'Email', elemento: document.getElementById('email') },{ valor: telefone, nome: 'Telefone', elemento: document.getElementById('telefone') },{ valor: motivo, nome: 'Motivo', elemento: document.getElementById('motivo') },];
+
+            let camposVazios = [];
+            let todosPreenchidos = true;
+
+            campos.forEach(campo => {
+                if (!campo.valor) {
+                    camposVazios.push(campo.nome);
+                    campo.elemento.style.borderColor = '#ff0000';
+                    todosPreenchidos = false;
+                } else {
+                    campo.elemento.style.borderColor = '#16bac5';
+                }
+            });
+
+            if(!encontrou.value){
+                encontrou = 'Não informado';
+            }else{
+                encontrou = document.getElementById('encontrou').value;
+            }
+
+
+            if (!todosPreenchidos) {
+                alert('Por favor, preencha todos os campos obrigatórios:\n- ' + camposVazios.join('\n- '));
+                return false;
+            }
+            
+            return true;
+        }
+
+        
