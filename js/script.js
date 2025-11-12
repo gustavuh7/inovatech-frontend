d9b4cdb7a270504dce4b48e8f8702039c6aae5cb
gustav <gustavuh0012@gmail.com>
2025-11-11 23:01:39 -0300

    fix: arrumando o js e o html do formulário

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         nome: document.getElementById('nome').value,
         email: document.getElementById('email').value,
         telefone: document.getElementById('telefone').value,
-        motivo: document.getElementById('motivo').value,
+        mensagem: document.getElementById('mensagem').value,
         encontrou : document.getElementById('encontrou').value || 'Não informado',
     };
 
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('formContato').reset();
             botao.disabled = false;
             botao.textContent = 'Enviar';
-            alert('Informações enviadas: \n' + 'Nome: '+ formData.nome + '\n' + 'Email: ' + formData.email + '\n' + 'Telefone: ' + formData.telefone + '\n' + 'Motivo: ' + formData.motivo + '\n' + 'Onde encontrou: ' + formData.encontrou);
+            alert('Informações enviadas: \n' + 'Nome: '+ formData.nome + '\n' + 'Email: ' + formData.email + '\n' + 'Telefone: ' + formData.telefone + '\n' + 'Mensagem: ' + formData.mensagem + '\n' + 'Onde encontrou: ' + formData.encontrou);
         })
         .catch((error) => {
             alert('Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente.', error);
